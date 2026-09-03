b8ef43c558421d5ae06ce1a3c7745304d07803cd
Arnav pratap singh <arnavpratapsingh2006@gmail.com>
2026-09-03 13:52:56 +0530

    callback hell

--- JavaScript/callbackHell.js
+++ JavaScript/callbackHell.js
@@ -1,13 +1,43 @@
-function test1(){
+// function test1(){
+//     setTimeout(()=>{
+//         console.log("task1")
+//     },4000)
+// }
+
+// function test2(){
+//     setTimeout(()=>{
+//         console.log("task2")
+//     },2000)
+// }
+// test1()
+// test2()
+
+function test1(cb){
+    setTimeout(()=>{
+        console.log("task1")
+        cb()
+    },4000)
+}
+function test2(cb){
+    setTimeout(()=>{
+        console.log("task2")
+        cb();
+    },2000)
+}
+function test3(cb){
     setTimeout(()=>{
         console.log("task1")
     },4000)
 }
-
-function test2(){
+function test4(cb){
     setTimeout(()=>{
-        console.log("task2")
-    },2000)
+        console.log("task1")
+    },100)
 }
-test1()
-test2()
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            test4();
+        })
+    })
+})
