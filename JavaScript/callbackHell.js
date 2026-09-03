a780cda7e2d89bb37c2efcc6b0ef9c4030701312
Arnav pratap singh <arnavpratapsingh2006@gmail.com>
2026-09-03 13:41:56 +0530

    callback hell

---
+++ JavaScript/callbackHell.js
@@ -0,0 +1,13 @@
+function test1(){
+    setTimeout(()=>{
+        console.log("task1")
+    },4000)
+}
+
+function test2(){
+    setTimeout(()=>{
+        console.log("task2")
+    },2000)
+}
+test1()
+test2()
